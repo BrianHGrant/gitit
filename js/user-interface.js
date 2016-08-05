b7f7b2562c1daa7bb7b4edd889855cf2ac2f3986
BrianHGrant <bhgrant8@gmail.com>
2016-08-05 10:43:38 -0700

    add ability to search for gitusers, front-end to display pages of 30 with avatar and username, ability to step through pages

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,11 +1,26 @@
 var User = require('./../js/user.js').userModule;
 
+
+var displaySearchResult = function(totalCount, avatarURL, login, pageLink, j) {
+  $('#gallery-row0').append('<div class="col-sm-2" id="gallery-user' + j +'"><img src="" alt="user avatar" class = "img-circle img-responsive" id="user-avatar' + j +'"><p class="name"id ="user-title' + j +'"></p></div>')
+  $('#showCount').text('Search Returned: ' + totalCount + ' Results');
+  $('#user-avatar' + j).attr('src', avatarURL);
+  $('#user-title' + j).text(login);
+};
 $(document).ready(function() {
+  var pageNumber = 0;
   $('#username-search').submit(function(event) {
     event.preventDefault();
     var userName = $('#userName').val();
-    console.log(userName)
+    console.log(userName);
     var currentUser = new User(userName);
-    currentUser.getEmail();
+    currentUser.searchName(pageNumber, displaySearchResult);
+    $('#next-page').submit(function(event) {
+      $('#gallery-row0').html("");
+      event.preventDefault();
+      pageNumber = pageNumber + 1;
+      console.log(pageNumber);
+      currentUser.searchName(pageNumber, displaySearchResult);
+    })
   });
 });
--- js/user.js
+++ js/user.js
@@ -4,9 +4,14 @@ function User(userName) {
   this.name = userName;
 }
 
-User.prototype.getEmail = function() {
-  $.get("https://api.github.com/users/" + this.name + "?access_token=" + apiKey).then(function(response){
-    console.log(response);
+User.prototype.searchName = function(pageNumber, displaySearchResult) {
+  $.get('https://api.github.com/search/users?q=' + this.name + '&access_token=' + apiKey + '&page=' + pageNumber).then(function(response){
+    console.log(pageNumber);
+    var total = response.items.length;
+    for(j=0; j<total; j++) {
+      displaySearchResult(response.total_count, response.items[j].avatar_url, response.items[j].login, response.items[j].html_url, j);
+      console.log(response.items[j].login);
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
