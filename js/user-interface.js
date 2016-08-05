513f33dc7cb9d5d7a5bf856e388ace8bb4f73924
BrianHGrant <bhgrant8@gmail.com>
2016-08-05 14:13:39 -0700

    add individual user view page

--- js/user-interface.js
+++ js/user-interface.js
@@ -2,25 +2,51 @@ var User = require('./../js/user.js').userModule;
 
 
 var displaySearchResult = function(totalCount, avatarURL, login, pageLink, j) {
-  $('#gallery-row0').append('<div class="col-sm-2" id="gallery-user' + j +'"><img src="" alt="user avatar" class = "img-circle img-responsive" id="user-avatar' + j +'"><p class="name"id ="user-title' + j +'"></p></div>')
+  $('#gallery-row0').append('<div class="col-sm-2 user-boxes" id="gallery-user' + j +'"><img src="" alt="'+ login +'" class = "img-circle img-responsive user-image" id="user-avatar' + j +'"><a href="' + pageLink +'"><p class="name"id ="user-title' + j +'"></p></a></div>')
   $('#showCount').text('Search Returned: ' + totalCount + ' Results');
   $('#user-avatar' + j).attr('src', avatarURL);
   $('#user-title' + j).text(login);
 };
+
+var displayUser = function(avatar_url, login, bio, created_at, location, hireable) {
+  $('#user-gallery').hide();
+  $('#gallery-header').hide();
+  $('#user-space').show();
+  $('#single-user-img').attr("src", avatar_url);
+  $('#single-user-title').text(login);
+  $('#single-user-bio').text(bio);
+  $('#single-user-created-at').text(created_at);
+  $('#single-user-location').text(location);
+  $('#single-user-hireable').text(hireable);
+}
+
+var runSearch = function(searchInput, pageNumber){
+  searchInput.searchName(pageNumber, displaySearchResult, displayUser);
+}
+
 $(document).ready(function() {
-  var pageNumber = 0;
+  var pageNumber = 1;
   $('#username-search').submit(function(event) {
+    $('#gallery-row0').html("");
+    $('#user-gallery').show();
+    $('#gallery-header').show();
     event.preventDefault();
     var userName = $('#userName').val();
-    console.log(userName);
     var currentUser = new User(userName);
-    currentUser.searchName(pageNumber, displaySearchResult);
+    runSearch(currentUser);
     $('#next-page').submit(function(event) {
       $('#gallery-row0').html("");
       event.preventDefault();
       pageNumber = pageNumber + 1;
-      console.log(pageNumber);
-      currentUser.searchName(pageNumber, displaySearchResult);
-    })
+      runSearch(currentUser);
+    });
+    $('#prev-page').submit(function(event) {
+      $('#gallery-row0').html("");
+      event.preventDefault();
+      if (pageNumber > 1) {
+        pageNumber = pageNumber - 1;
+      }
+      runSearch(currentUser);
+    });
   });
 });
--- js/user.js
+++ js/user.js
@@ -4,14 +4,35 @@ function User(userName) {
   this.name = userName;
 }
 
-User.prototype.searchName = function(pageNumber, displaySearchResult) {
+getUser = function(user, displayFunction) {
+  $.get('https://api.github.com/users/'+ user +'?access_token=' + apiKey).then(function(response){
+    displayFunction(response.avatar_url, response.login, response.bio, response.created_at, response.location, response.hireable);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+getRepos = function(user, displayFunction) {
+  $.get('https://api.github.com/users/' + this.name + '&access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+User.prototype.searchName = function(pageNumber, displaySearchResult, displayUser, displayRepos) {
   $.get('https://api.github.com/search/users?q=' + this.name + '&access_token=' + apiKey + '&page=' + pageNumber).then(function(response){
     console.log(pageNumber);
     var total = response.items.length;
     for(j=0; j<total; j++) {
       displaySearchResult(response.total_count, response.items[j].avatar_url, response.items[j].login, response.items[j].html_url, j);
-      console.log(response.items[j].login);
     }
+  }).then(function(response){
+    $('.user-image').click(function(event) {
+      var searchUser = this.alt;
+      getUser(searchUser, displayUser);
+      getRepos(searchUser, displayRepos);
+    });
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
